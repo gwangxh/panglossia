c90234911f8bb4f1b389bb1532a8a10d147ec620
gwangxh <64791836+gwangxh@users.noreply.github.com>
2024-02-11 16:55:09 -0600

    Update soothill.js

--- soothill.js
+++ soothill.js
@@ -30,8 +30,11 @@ document.querySelectorAll('.lookup').forEach(function(element) {
     var searchTerm = element.textContent.trim();
     var xmlFileUrl = 'ddbc.soothill-hodous.tei.p5.xml'; // Replace 'path_to_your_xml_file' with the actual path
     loadXMLFile(xmlFileUrl, function(xmlDoc) {
+      var boundingRect = element.getBoundingClientRect();
+      var clickX = boundingRect.left + window.scrollX;
+      var clickY = boundingRect.top + window.scrollY + element.offsetHeight;
       var searchResults = searchDictionary(xmlDoc, searchTerm);
-      displayResults(searchResults, event.clientX, event.clientY);
+      displayResults(searchResults, clickX, clickY);
     });
   });
 });
